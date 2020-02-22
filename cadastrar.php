4d6c975d9d3fd080a4ef7cfe150de32f14ec42a2
Andrew Ribeiro <andrew.ribeiro@etec.sp.gov.br>
2020-02-21 21:56:46 -0300

    Criação de arquivo de login e cadastro para demonstração de métodos HTTP GET E POST

---
+++ cadastrar.php
@@ -0,0 +1,9 @@
+<meta charset="utf-8"/>
+<?php 
+
+    $nome = $_POST["nome"];
+    $cpf = $_POST["cpf"];
+    $idade = $_POST["idade"];
+
+    echo "$nome, você foi cadastrado com sucesso!";
+?>
---
+++ logar.php
@@ -0,0 +1,10 @@
+<meta charset="utf-8"/>
+
+<?php
+    
+    $loginDoUsuario = $_POST["login"];
+    $senhaDoUsuario = $_POST["pass"];
+    $filtro = $_GET["filtro"];
+
+    echo "Bem vindo, $loginDoUsuario, você está na $filtro";
+?>
